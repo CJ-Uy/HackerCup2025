bf630e2b8bfda611719d2508a3eda8fc4fcf4d51
Kurt Oswill <kurtoswillmc@gmail.com>
2025-08-16 07:54:56 +0800

    feat: activity

--- src/app/activity/page.tsx
+++ src/app/activity/page.tsx
@@ -1,3 +1,15 @@
-export default function Activity() {
-	return <h1>Activity</h1>;
-}
+import React from 'react';
+import ActivityCard from "@/components/ActivityCard";
+
+const Page = () => {
+    return (
+        <div>
+			<h1 className="mb-5 text-2xl font-semibold">Activity</h1>
+
+			{/*Sample Activities*/}
+			<ActivityCard />
+        </div>
+    );
+};
+
+export default Page;
